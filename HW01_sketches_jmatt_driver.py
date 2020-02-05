9531d13ba1091154a258d05e1d3bd39a8102078d
Jeremy Matt <jmatt@uvm.edu>
2020-02-05 06:13:40 -0500

    HW01 jm arch

--- HW01_sketches_jmatt_driver.py
+++ HW01_sketches_jmatt_driver.py
@@ -114,7 +114,7 @@ model.add(layers.Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics = ['accuracy'])
 
-nb_epochs = 25
+nb_epochs = 10
 history = model.fit_generator(
     train_generator,
     steps_per_epoch = train_generator.samples // batch_size,

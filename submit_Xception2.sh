0ad741bd3a79eb3d59d46d862e58032361468f5b
Jeremy Matt <jmatt@uvm.edu>
2020-02-16 19:01:28 -0500

    HW01 sbatch submit files

---
+++ submit_Xception2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# specify a partition
+#SBATCH --partition=dggpu
+# Request nodes
+#SBATCH --nodes=1
+# Request some processor cores
+#SBATCH --ntasks=32
+# Send Notification email
+#SBATCH	--mail-type=FAIL,END
+#SBATCH --mail-user=jmatt@uvm.edu
+# Request GPUs
+#SBATCH --gres=gpu:1
+# Request memory 
+#SBATCH --mem=32G
+# Maximum runtime of 15 hrs
+#SBATCH --time=30:00:00
+# Name of this job
+#SBATCH --job-name=xc_200_FT
+# Output of this job, stderr and stdout are joined by default
+# %x=job-name %j=jobid
+#SBATCH --output=../output/%x_%j.out
+# change to the directory where you submitted this script
+cd ${SLURM_SUBMIT_DIR}
+# your job execution follows:
+time python ~/scratch/DeepLearning/github/CS395_homework/HW01_jmatt_Xception2.py
+
+

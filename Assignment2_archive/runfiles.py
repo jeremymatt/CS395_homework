e683b03a47c9858ec7f8563a6f417d600f8d5053
Jeremy Matt <jmatt@uvm.edu>
2020-04-04 11:03:47 -0400

    Archive Project 2

---
+++ Assignment2_archive/runfiles.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 00:04:40 2020
+
+@author: jmatt
+"""
+
+print('RUNNING GRU')
+runfile('D:/Github/CS395_homework/GRU_driver.py', wdir='D:/Github/CS395_homework')
+print('RUNNING LSTM')
+runfile('D:/Github/CS395_homework/LSTM_driver.py', wdir='D:/Github/CS395_homework')
+print('RUNNING RNN')
+runfile('D:/Github/CS395_homework/RNN_driver.py', wdir='D:/Github/CS395_homework')
